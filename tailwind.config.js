6576e7260987652c53d98d246e56320be2f7cc4f
sergio.valencia <sergio.valencia@exos.mx>
2024-04-18 17:01:49 -0600

    fit first tailwind styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,16 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
+    screens: {
+      sm: "480px",
+      md: "768px",
+      lg: "1080px",
+    },
+    colors: {
+      black: "hsl(210, 13%, 3%)",
+      white: "hsl(0, 0%, 100%)",
+      green: "hsl(120, 43%, 27%)"
+    },
     extend: {},
   },
   plugins: [],
